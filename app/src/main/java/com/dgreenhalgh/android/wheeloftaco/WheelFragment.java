1f6b9c1b872f332cc506618e2d1732903327b987
David <davidrgreenhalgh@gmail.com>
2014-07-25 13:05:45 -0400

    Single fragment foundation

---
+++ app/src/main/java/com/dgreenhalgh/android/wheeloftaco/WheelFragment.java
@@ -0,0 +1,15 @@
+package com.dgreenhalgh.android.wheeloftaco;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class WheelFragment extends Fragment {
+
+}
