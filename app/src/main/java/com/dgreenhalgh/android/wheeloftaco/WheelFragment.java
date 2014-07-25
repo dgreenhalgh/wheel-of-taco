483e32364e9d687f50661817db58a0131da6310e
David <davidrgreenhalgh@gmail.com>
2014-07-25 13:49:55 -0400

    Laid foundation for action bar/options menu

--- app/src/main/java/com/dgreenhalgh/android/wheeloftaco/WheelFragment.java
+++ app/src/main/java/com/dgreenhalgh/android/wheeloftaco/WheelFragment.java
@@ -4,12 +4,38 @@ package com.dgreenhalgh.android.wheeloftaco;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WheelFragment extends Fragment {
+
+    private TextView mResultTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wheel, parent, false);
+
+        mResultTextView = (TextView)view.findViewById(R.id.result_text_view);
+        mResultTextView.setText(R.string.result_text_view_default);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.restaurant, menu);
+    }
 
 }
